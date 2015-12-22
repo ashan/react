1f59f2db00fd7e92ae2468510bf15f5acb6d77b8
unknown <conak2@D-51300XF.fmg.net>
2015-12-22 14:41:37 +1300

    ToDo initial commit

---
+++ ToDo/src/app.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
+var Header = require('./header');
+
+var rootUrl = 'https://ashan-todos.firebaseio.com/';
+
+var App = React.createClass({
+	mixins: [ReactFire],
+
+	componentWillMount: function(){
+		this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+	},
+
+	render: function() {
+		return <div className="row ">
+			<div className="col-md-8 col-md-offset-2">
+				<h2 className="text-center">
+					To-Do List
+				</h2>
+				<Header/>
+			</div>
+		</div>
+	}
+});
+
+var element = React.createElement(App, {});
+React.render(element, document.querySelector('.container'));
